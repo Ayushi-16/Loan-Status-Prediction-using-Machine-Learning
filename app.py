97386b716d886a3cb5154bcb7662faddbda47da5
Ayushi-16 <ayushigarach.it20@scet.ac.in>
2023-03-21 15:26:55 +0530

    2nd commit

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ app=flask.Flask(__name__, template_folder='template')
 @app.route('/',methods=['GET','POST'])
 def main():
     if flask.request.method == 'POST':
+        gender=float(flask.request.form['gender'])
+        married=float(flask.request.form['married'])
+        dependents=float(flask.request.form['dependents'])
+        education=float(flask.request.form['education'])
+        employed=float(flask.request.form['employed'])
+        applicant_income=float(flask.request.form['applicant_income'])
+        coapplicant_income=float(flask.request.form['coapplicant_income'])
+        loan_amount=float(flask.request.form['loan_amount'])
+        loan_amount_term=float(flask.request.form['loan_amount_term'])
+        credit=float(flask.request.form['credit'])
+        p_area=float(flask.request.form['p_area'])
         model=pickle.load(open('bin/model.pkl','rb'))
-        gender=flask.request.form['gender']
-        married=flask.request.form['married']
-        dependents=flask.request.form['dependents']
-        education=flask.request.form['education']
-        employed=flask.request.form['employed']
-        applicant_income=flask.request.form['applicant_income']
-        coapplicant_income=flask.request.form['coapplicant_income']
-        loan_amount=flask.request.form['loan_amount']
-        loan_amount_term=flask.request.form['loan_amount_term']
+        return flask.render_template('index.html',loan_status=int(model.predict([[gender,married,dependents,education,employed,applicant_income,coapplicant_income,loan_amount,loan_amount_term,credit,p_area]])))
+    else:
+        return flask.render_template('index.html')
